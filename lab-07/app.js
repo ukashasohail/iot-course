a6f54fd771780fda1d6f5b89f6e91b79107886b3
ukashasohail <ukasha909@outlook.com>
2019-01-27 11:03:58 +0500

    lab07

---
+++ lab-07/app.js
@@ -0,0 +1,11 @@
+var currentDate = new Date()
+
+var x = currentDate.toString();
+console.log(x);
+
+var y = currentDate.getDay()
+console.log(y);
+
+console.log(currentDate.getHours());
+console.log(currentDate.getMinutes());
+
