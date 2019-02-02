3ed741d507799fd1316ee97dc837a56352758719
ukashasohail <ukasha909@outlook.com>
2019-02-02 21:25:03 +0500

    2

---
+++ p21-42/28-30.js
@@ -0,0 +1,18 @@
+//ch28-29
+var x = "1.999";
+var y = "10";
+var z = 160;
+
+console.log(parseFloat(x));
+console.log(parseInt(y));
+
+console.log(Number(x),Number(y));
+
+console.log(z.toString());
+
+//ch30
+var foo = 1.36564;
+var foo1 = foo.toFixed(2);
+var foo2 = foo.toFixed();
+
+console.log(foo1,foo2);
